d69c419d92d9bca78ae29855305056afdff5b197
Frank-MI <miyu3310639@126.com>
2019-01-14 11:10:52 -0500

    add 2B

---
+++ 2B/2B.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <cstring>
+
+#define INPUT_REDIRECTION
+#define MAXN 1010
+
+
+
+int main()
+{
+#ifdef INPUT_REDIRECTION
+	freopen("in.in","r",stdin);
+#endif
+
+    return 0;
+}
