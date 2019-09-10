c4106635c40bc3424ddad360caa8c60e41cdeb33
Frank-MI <miyu3310639@126.com>
2019-09-10 11:33:55 -0400

    create 5C

---
+++ Contest5/5C/5C.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+#define MAXN (1000000 + 50)
+char line[MAXN];
+
+int main()
+{
+    #ifndef USE_ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    while (std::cin.getline(line,MAXN))
+    {
+        
+    }
+
+    return 0;
+}
