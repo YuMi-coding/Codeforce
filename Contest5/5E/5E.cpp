e6d9086e1ece331cd6116048a9edcb35ea9fdb7f
Frank-MI <miyu3310639@126.com>
2020-10-29 14:25:59 -0400

    complete 5E

--- Contest5/5E/5E.cpp
+++ Contest5/5E/5E.cpp
@@ -6,6 +6,46 @@
 #include <cmath>
 
 #define USE_ONLINE_JUDGE
+#define MAXN (1000000 + 5)
+int n;
+int input_sauce[MAXN];
+int height[MAXN];
+int l[MAXN];
+int r[MAXN];
+int c[MAXN];
+
+bool input(){
+    memset(height, 0, sizeof(height));
+    memset(l, 0, sizeof(l));
+    memset(r, 0, sizeof(r));
+    memset(c, 0, sizeof(c));
+    if (scanf("%d", &n)== EOF){
+        return false;
+    }
+    int i = 0;
+    for(i = 0; i< n; i++)
+        scanf("%d", &input_sauce[i]);
+    return true;
+}
+
+int adjust_input(){
+    int max_pos = 0, max_val = -1;
+    for (int i = 0; i<n; i++){
+        if(input_sauce[i] > max_val){
+            max_val = input_sauce[i];
+            max_pos = i;
+        }
+    }
+    for(int i = 0; i<n; i++){
+        height[i] = input_sauce[(i+max_pos)%n];
+        #ifndef USE_ONLINE_JUDGE
+            printf("pos = %d, height = %d.\n", i, height[i]);
+        #endif
+    }
+    height[n] = max_val;
+
+    return 0;
+}
 
 int main()
 {
@@ -14,93 +54,45 @@ int main()
         freopen("output.txt","w",stdout);
     #endif
 
-    while(scanf("%lf%lf%lf%lf%lf",&a,&v,&l,&d,&w)!=EOF)
+    while(input())
     {
-        double maxAccelTime = v * 1.0 / a;
-        double maxAccelDist = 0.5 * v * v / a;
-        double ans = 0.0;
-        #ifndef USE_ONLINE_JUDGE
-            printf("maxAccelTime = %f, maxAccelDist = %f\n",maxAccelTime,maxAccelDist);
-        #endif
-        if(maxAccelDist > d)// Does not accelerate to top speed when reach the speed sign
+        adjust_input();
+        long long ans = 0;
+        for(int i = n; i>=0; --i)
         {
-no_time_to_decel:
-            double timeReachSign = sqrt(double(2.0 * d)/a);
-            double speedReachSign = sqrt(double(2.0 * a * d));
+            r[i] = i+1;
+            while (r[i] < n && height[i] > height[r[i]])
+                r[i] = r[r[i]];
+            if (r[i] < n && height[i] == height[r[i]]) {
+                c[i] = c[r[i]] + 1;
+                r[i] = r[r[i]];
+            }
+        }
+        for(int i = 0; i<n ;i++){
+            l[i] = i-1;
+            while (l[i] > 0 && height[i] > height[l[i]])
+                l[i] = l[l[i]];
+            if (l[i] > 0 && height[i] == height[l[i]]){
+                // c[i] = c[l[i]] + 1;
+                l[i] = l[l[i]];
+            }
+        }
+        for(int i = 1; i<n;i++)
+        {
+            int addition = c[i];
+            if (l[i] == 0 && r[i] == n){
+                addition = (1+c[i]);
+            }
+            else{
+                addition = (2+c[i]);
+            }
             #ifndef USE_ONLINE_JUDGE
-                printf("timeReachSign=%f,speedReachSign=%f\n",timeReachSign,speedReachSign);
+                printf("pos = %d , height = %d, left = %d, right = %d, addition = %d.\n", i, height[i], \
+                l[i], r[i], addition);
             #endif
-            if(speedReachSign < w) //Does not need to decelerate
-            {
-                if(l < maxAccelDist){ // No enough space for accelerating
-                    ans = sqrt(double(2.0*l)/a);
-                }
-                else{
-                    ans = maxAccelTime + (l - maxAccelDist)/double(v);
-                }
-            }
-            else // Need to decelerate
-            {
-                double speedDifference = speedReachSign - w;
-                double timeDecreaseSpeed = speedDifference / double(2 * a);
-                #ifndef USE_ONLINE_JUDGE
-                    printf("speedDifferene = %f,timeDecreaseSpeed = %f\n",speedDifference,timeDecreaseSpeed);
-                #endif
-                double timeAccel = sqrt((2*a*d+w*w)/(2*a*a));
-                #ifndef USE_ONLINE_JUDGE
-                    printf("timeAccel = %f\n",timeAccel);
-                #endif
-                double ansFirst = timeAccel + (timeAccel*a-w)/a;
-
-                double ansSecond = 0; // To be filled in following
-                double distanceRemain = l - d;
-                double toTopSpeedDistance = (double(w) + double(v))/2 * (double(v) - double(w))/a;
-                if(toTopSpeedDistance < distanceRemain)
-                {// We can achieve top speed in the remaining distance
-                    ansSecond = (distanceRemain-toTopSpeedDistance)/double(v) + (double(v) - double(w))/a;
-                }
-                else
-                {// We cannot achieve top speed in the remaining distance
-                    ansSecond = (sqrt(4.0 * double(w) * double(w) + 8 * a * distanceRemain) - 2.0*double(w))/(2*a);
-                }
-                #ifndef USE_ONLINE_JUDGE
-                    printf("ansFirst = %f, ansSecond = %f\n", ansFirst, ansSecond);
-                #endif
-                ans = ansFirst + ansSecond;
-            }
+            ans += addition;
         }
-        else // Accelerates to top speed when reach speed sign
-        {
-            if(v < w)
-            {
-                #ifndef USE_ONLINE_JUDGE
-                    printf("v < w\n");
-                #endif
-                ans = (l - maxAccelDist) / v + maxAccelTime;
-            }
-            else
-            {
-                #ifndef USE_ONLINE_JUDGE
-                    printf("v > w\n");
-                #endif
-                double speedDifference = v - w;
-                double timeDecreaseSpeed = speedDifference / double(a);
-                double distanceDecrease = v * timeDecreaseSpeed - 0.5 * a * timeDecreaseSpeed * timeDecreaseSpeed;
-                double distanceNoChangeSpeed = d - maxAccelDist - distanceDecrease;
-                #ifndef USE_ONLINE_JUDGE
-                    printf("distanceDecrease = %f, distanceNoChangeSpeed = %f\n", distanceDecrease, distanceNoChangeSpeed);
-                #endif
-
-                if(distanceNoChangeSpeed < 0){// No enough space for decelerating
-                    goto no_time_to_decel;
-                }
-                else{
-                    ans = maxAccelTime + distanceNoChangeSpeed/v + timeDecreaseSpeed + timeDecreaseSpeed + (l-d-distanceDecrease)/v;
-                }
-            }
-        }
-
-        printf("%.6f\n",ans);
+        printf("%lld\n", ans);
     }
     return 0;
 }
